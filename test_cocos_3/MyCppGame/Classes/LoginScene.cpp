4cdbc469410c8821f47e338780381f19c42da466
yangqingluo <84807330@qq.com>
2016-12-07 16:09:19 +0800

    注册登录框初步实现

--- test_cocos_3/MyCppGame/Classes/LoginScene.cpp
+++ test_cocos_3/MyCppGame/Classes/LoginScene.cpp
@@ -9,6 +9,7 @@
 #include "LoginScene.h"
 
 USING_NS_CC;
+using namespace ui;
 
 Scene* LoginScene::createScene()
 {
@@ -58,10 +59,43 @@ bool LoginScene::init()
     btn_BackItem->setScale(this->getScaleX(), this->getScaleY());
     btn_BackItem->setPosition(Vec2(origin.x + btn_BackItem->getContentSize().width, origin.y + visibleSize.height - btn_BackItem->getContentSize().height));
     
+    auto closeItem = MenuItemImage::create(
+                                           "UIRes/visible.png",
+                                           "UIRes/visible.png",
+                                           CC_CALLBACK_1(LoginScene::btVisbleCallback, this));
+    
+    closeItem->setPosition(Vec2(origin.x + visibleSize.width  - closeItem->getContentSize().width/2 ,
+                                origin.y + visibleSize.height - closeItem->getContentSize().height/2));
+    
     // create menu, it's an autorelease object
-    auto menu = Menu::create(btn_BackItem, NULL);
+    auto menu = Menu::create(btn_BackItem, closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
+    
+    
+    
+    /*Frame的测试*/
+    Widget* frame = cocostudio::GUIReader::getInstance()->widgetFromJsonFile("UIRes/DemoLogin.ExportJson");
+    frame->setScale(0.8f); //缩放比例可以随便设置
+    frame->setAnchorPoint(Vec2(0.5f,0.5f));//锚点可以随便设置
+    frame->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));//坐标可以随便设置
+    this->addChild(frame);
+    
+    //触碰测试
+    Helper::seekWidgetByName(frame,"login_Button")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    Helper::seekWidgetByName(frame,"close_Button")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    Helper::seekWidgetByName(frame,"agree_CheckBox")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    Helper::seekWidgetByName(frame,"name_TextField")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    Helper::seekWidgetByName(frame,"password_TextField")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    Helper::seekWidgetByName(frame,"confirm_TextField")->addTouchEventListener(CC_CALLBACK_2(LoginScene::btMenuCallBack,this));
+    
+    //frame初始化,可以试一下不初始化，直接layer->setVisible(false);设置窗口不可见之后,鼠标点击原本的按钮区域，看看是否有对话框出现
+    m_frame.setFrame(frame,Helper::seekWidgetByName(frame,"backageImg"));
+    
+    //设置点击在对话框外部时候的监听事件，可以在这里将对话框隐藏
+    m_frame.setTouchOutOfFrameEventListener(CC_CALLBACK_1(LoginScene::touchOutOfFrame,this));
+    
+    m_frame.setFrameVisible(true);
     
     
     return true;
@@ -74,17 +108,29 @@ void LoginScene::buttonCallback(cocos2d::Ref* pSender, int index){
         }
             break;
             
-        case 1:{
-            
-        }
             break;
             
-        case 2:{
-            
+        default:{
         }
-            break;
-            
-        default:
             break;
     }
+}
+
+void LoginScene::btMenuCallBack(Ref* pObject,Widget::TouchEventType eventType)
+{
+    if(eventType == Widget::TouchEventType::ENDED)
+    {
+        MessageBox("touch ui widget","touch ui widget");
+    }
+}
+
+void LoginScene::btVisbleCallback(Ref* pSender)
+{
+    m_frame.setFrameVisible(true);
+}
+
+void LoginScene::touchOutOfFrame(cocos2d::ui::Widget*)
+{
+    //触碰点在对话框外，将对话框隐藏
+    m_frame.setFrameVisible(false);
 }
--- test_cocos_3/MyCppGame/Classes/LoginScene.h
+++ test_cocos_3/MyCppGame/Classes/LoginScene.h
@@ -10,6 +10,7 @@
 #define LoginScene_h
 
 #include "cocos2d.h"
+#include "FrameManager.h"
 
 class LoginScene : public cocos2d::Layer
 {
@@ -19,9 +20,15 @@ public:
     virtual bool init();
     
     void buttonCallback(cocos2d::Ref* pSender, int index);
+    void touchOutOfFrame(cocos2d::ui::Widget*);
     
+    void btVisbleCallback(cocos2d::Ref*);
+    void btMenuCallBack(cocos2d::Ref*,cocos2d::ui::Widget::TouchEventType);
     // implement the "static create()" method manually
     CREATE_FUNC(LoginScene);
+    
+private:
+    FrameManager m_frame;
 };
 
 #endif /* LoginScene_h */
