31ce142bf21f1c1e0c283590b3ff3c9118abecaf
yangqingluo <84807330@qq.com>
2016-12-19 10:14:54 +0800

    socket连接测试

--- test_cocos_3/MyCppGame/Classes/HelloWorldScene.cpp
+++ test_cocos_3/MyCppGame/Classes/HelloWorldScene.cpp
@@ -150,30 +150,30 @@ bool HelloWorld::init()
 }
 
 void HelloWorld::loginCallback(cocos2d::Ref* pSender, int index){
-    auto scene = Hall::createScene();
-    Director::getInstance()->pushScene(scene);
-//    switch (index) {
-//        case 0:{
-//            auto scene = Hall::createScene();
-//            Director::getInstance()->pushScene(scene);
-//        }
-//            break;
-//            
-//        case 1:{
-//            
-//        }
-//            break;
-//            
-//        case 2:{
-//            auto scene = LoginScene::createScene();
-//            
-//            Director::getInstance()->pushScene(scene);
-//        }
-//            break;
-//            
-//        default:
-//            break;
-//    }
+//    auto scene = Hall::createScene();
+//    Director::getInstance()->pushScene(scene);
+    switch (index) {
+        case 0:{
+            auto scene = Hall::createScene();
+            Director::getInstance()->pushScene(scene);
+        }
+            break;
+            
+        case 1:{
+            connectServer();
+        }
+            break;
+            
+        case 2:{
+            auto scene = LoginScene::createScene();
+            
+            Director::getInstance()->pushScene(scene);
+        }
+            break;
+            
+        default:
+            break;
+    }
 }
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
@@ -191,4 +191,53 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
     //_eventDispatcher->dispatchEvent(&customEndEvent);
     
     
+}
+
+
+// Socker连接
+void HelloWorld::connectServer()
+{
+    // 初始化
+    // ODSocket socket;
+    socket.Init();
+    socket.Create(AF_INET, SOCK_STREAM, 0);
+    
+    // 设置服务器的IP地址，端口号
+    // 并连接服务器 Connect
+    const char* ip = "10.128.31.231";
+    int port = 8888;
+    bool result = socket.Connect(ip, port);
+    
+    // 发送数据 Send
+//    socket.Send("{\"id\":1000}", 5);
+    
+    if (result) {
+        CCLOG("connect to server success!");
+        // 开启新线程，在子线程中，接收数据
+        std::thread recvThread = std::thread(&HelloWorld::receiveData, this);
+        recvThread.detach(); // 从主线程分离
+    }
+    else {
+        CCLOG("can not connect to server");
+        return;
+    }
+}
+
+// 接收数据
+void HelloWorld::receiveData()
+{
+    // 因为是强联网
+    // 所以可以一直检测服务端是否有数据传来
+    while (true) {
+        // 接收数据 Recv
+        char data[512] = "";
+        int result = socket.Recv(data, 512, 0);
+        printf("%d", result);
+        // 与服务器的连接断开了
+        if (result <= 0) break;
+        
+        CCLOG("%s", data);
+    }
+    // 关闭连接
+    socket.Close();
 }
